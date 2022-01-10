47bf971b4eb33e82eae7d16d22643c4a5ab7b029
Martin Storgaard <ms@ynk.gl>
2022-01-10 20:31:04 +0100

    Added blacklist handler

--- src/handlers/blacklist-handler.ts
+++ src/handlers/blacklist-handler.ts
@@ -6,8 +6,14 @@ export class BlacklistConfiguration {
    *
    */
   constructor(
-    readonly caseInsensitiveWords?: string[],
-    readonly regExps?: RegExp[]
+    /**
+     * A list of blacklisted words. Both the password and every words are compared with toLocaleUpperCase.
+     */
+    readonly caseInsensitiveWords: string[],
+    /**
+     * A list of blacklisted regular expressions. Each expression is compared with the original password.
+     */
+    readonly regExps: RegExp[]
   ) {}
 }
 
@@ -17,43 +23,63 @@ export class BlacklistHandler implements Handler {
   constructor(private readonly config: BlacklistConfiguration) {}
 
   public handle(password: string): Result {
-    const lowerPassword = password.toLowerCase();
-    if (this.config.caseInsensitiveWords) {
-      for (const entry in this.config.caseInsensitiveWords) {
-        if (
-          Object.prototype.hasOwnProperty.call(
-            this.config.caseInsensitiveWords,
-            entry
-          )
-        ) {
-          const word = this.config.caseInsensitiveWords[entry];
-          if (lowerPassword.indexOf(word) > 0) {
-            return Result.fail({
-              handler: this.name,
-              rule: 'caseInsensitiveWords',
-              expected: 0,
-              actual: 1,
-            });
-          }
-        }
-      }
+    const upperCasePassword = password.toLocaleUpperCase();
+    const containsCaseInsensitveWord = this.config.caseInsensitiveWords.some(
+      word => upperCasePassword.indexOf(word.toLocaleUpperCase()) >= 0
+    );
+    if (containsCaseInsensitveWord) {
+      return Result.fail({
+        handler: this.name,
+        rule: 'caseInsensitiveWords',
+        expected: 0,
+        actual: 1,
+      });
     }
 
-    if (this.config.regExps) {
-      for (const entry in this.config.regExps) {
-        if (Object.prototype.hasOwnProperty.call(this.config.regExps, entry)) {
-          const regExp = this.config.regExps[entry];
-          if (regExp.test(password)) {
-            return Result.fail({
-              handler: this.name,
-              rule: 'regExps',
-              expected: 0,
-              actual: 1,
-            });
-          }
-        }
-      }
+    const matchesRegExp = this.config.regExps.some(regExp =>
+      regExp.test(password)
+    );
+    if (matchesRegExp) {
+      return Result.fail({
+        handler: this.name,
+        rule: 'regExps',
+        expected: 0,
+        actual: 1,
+      });
     }
+
     return Result.ok(password);
+  }
+
+  /**
+   * addEmailInformation
+   */
+  public addEmailInformation(
+    email: string,
+    slidingWindowSize = 0,
+    minTokenLength = 2
+  ): void {
+    const tokens = email.split(/[@!#$%&'*+-/=?^_`{|]+/);
+    tokens.pop();
+
+    tokens.forEach(token => this.addToken(token, minTokenLength));
+
+    if (slidingWindowSize > 0) {
+      tokens.forEach(token => {
+        const maxIndex = token.length - slidingWindowSize + 1;
+        for (let index = 0; index < maxIndex; index++) {
+          this.addToken(
+            token.substring(index, index + slidingWindowSize),
+            minTokenLength
+          );
+        }
+      });
+    }
+  }
+
+  private addToken(token: string, minLength: number) {
+    if (token.length > minLength) {
+      this.config.caseInsensitiveWords.push(token);
+    }
   }
 }
--- test/handlers/blacklist-handler.test.ts
+++ test/handlers/blacklist-handler.test.ts
@@ -4,16 +4,27 @@ import {
 } from '../../src/handlers/blacklist-handler';
 // https://xkcd.com/936/
 const config = new BlacklistConfiguration(
-  ['troubad'],
+  ['troubaD'],
   [new RegExp(/tr[o0]ub[a4]dor[u&][r3]/i)]
 );
 const handler = new BlacklistHandler(config);
 
 describe('a valid password', () => {
+  const password = 'MyPassword';
   it('should return the original password in the result.', () => {
-    const password = 'MyPassword';
-
     const result = handler.handle(password);
+    expect(result.isSuccess).toBeTruthy();
+    expect(result.getPassword()).toBe(password);
+  });
+
+  it('should be valid when no case insensitive words are provided.', () => {
+    const emptyHandler = new BlacklistHandler({
+      caseInsensitiveWords: [],
+      regExps: [],
+    });
+
+    const result = emptyHandler.handle(password);
+
     expect(result.isSuccess).toBeTruthy();
     expect(result.getPassword()).toBe(password);
   });
@@ -30,6 +41,16 @@ describe('an invalid password', () => {
     expect(failures[0].actual).toBe(1);
   });
 
+  it('should reject banned words when the password starts with it.', () => {
+    const result = handler.handle('trouBADour');
+    const failures = result.getFailures();
+
+    expect(failures).toHaveLength(1);
+    expect(failures[0].rule).toBe('caseInsensitiveWords');
+    expect(failures[0].expected).toBe(0);
+    expect(failures[0].actual).toBe(1);
+  });
+
   it('should reject banned regular expressions.', () => {
     const result = handler.handle('tr0ub4dor&3');
     const failures = result.getFailures();
@@ -38,5 +59,78 @@ describe('an invalid password', () => {
     expect(failures[0].rule).toBe('regExps');
     expect(failures[0].expected).toBe(0);
     expect(failures[0].actual).toBe(1);
+  });
+});
+
+describe('when adding an e-mail to the blacklist', () => {
+  it('should add all tokens to the blacklist.', () => {
+    const emptyHandler = new BlacklistHandler({
+      caseInsensitiveWords: [],
+      regExps: [],
+    });
+
+    emptyHandler.addEmailInformation('john-doe@company.example.com');
+    ['john', 'doe', 'company', 'example'].forEach(password => {
+      const result = emptyHandler.handle(password);
+
+      const failures = result.getFailures();
+      expect(failures).toHaveLength(1);
+      expect(failures[0].rule).toBe('caseInsensitiveWords');
+      expect(failures[0].expected).toBe(0);
+      expect(failures[0].actual).toBe(1);
+    });
+  });
+
+  it('should allow the tld to be used.', () => {
+    const emptyHandler = new BlacklistHandler({
+      caseInsensitiveWords: [],
+      regExps: [],
+    });
+    emptyHandler.addEmailInformation('john-doe@company.example.com');
+
+    const result = emptyHandler.handle('com');
+
+    expect(result.isSuccess).toBeTruthy();
+  });
+
+  it('should block partial words based on a sliding window.', () => {
+    const emptyHandler = new BlacklistHandler({
+      caseInsensitiveWords: [],
+      regExps: [],
+    });
+    emptyHandler.addEmailInformation('john-doe@company.example.com', 5);
+
+    [
+      'john',
+      'doe',
+      'company',
+      'example',
+      'compa',
+      'ompan',
+      'mpany',
+      'examp',
+      'xampl',
+      'ample',
+    ].forEach(password => {
+      const result = emptyHandler.handle(password);
+
+      const failures = result.getFailures();
+      expect(failures).toHaveLength(1);
+      expect(failures[0].rule).toBe('caseInsensitiveWords');
+      expect(failures[0].expected).toBe(0);
+      expect(failures[0].actual).toBe(1);
+    });
+  });
+
+  it('should allow short words in the e-mail.', () => {
+    const emptyHandler = new BlacklistHandler({
+      caseInsensitiveWords: [],
+      regExps: [],
+    });
+    emptyHandler.addEmailInformation('john-doe@eu.example.com', undefined, 2);
+
+    const result = emptyHandler.handle('eu');
+
+    expect(result.isSuccess).toBeTruthy();
   });
 });
